7803569b013e042d0e5009291f35d3d1b6970668
crawforda <31389347+crawforda@users.noreply.github.com>
2017-10-15 12:26:09 -0400

    Add files via upload

---
+++ lab 5.js
@@ -0,0 +1,52 @@
+<meta charset="UTF-8">
+    <title>Title</title>
+    </head>
+    <body>
+
+    var createElement
+
+<div id="fiddle">
+
+    </div>
+
+p
+pElement.setAttribute ("id")
+
+li
+{
+    font-style: italic;
+}
+var divNode= document.getElemnt
+
+console.clear();
+
+getElementById
+
+element id= fiddle
+
+nod
+
+var= divNode
+PNode
+createElement
+p node
+
+createTextNode
+createElement (div)
+CSCI 215: Lab 5
+pTextNod
+
+pElement
+pNode (div)
+
+pNode appendChild
+
+createElement <hr>
+
+createElement
+
+
+
+
+</body>
+    </html>
